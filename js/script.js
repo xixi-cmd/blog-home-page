8cb3b4dcbce8221ae7cd6cd344cf520eca868831
Shelby <63186905+xixi-cmd@users.noreply.github.com>
2021-01-27 12:06:00 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+    let box = document.querySelectorAll(".box");
+    for (let i = 0; i < box.length; i++) {
+        box[i].onmouseenter = function (event) {
+            let ripple = box[i].querySelector(".ripple");
+            ripple.classList.add("animation");
+            ripple.style.width = this.offsetWidth + "px";
+            ripple.style.height = this.offsetWidth + "px";
+            ripple.style.top = -(this.offsetHeight - event.offsetY) + "px";
+            ripple.style.left = -(this.offsetWidth / 2 - event.offsetX) + "px";
+            setTimeout(function () {
+                ripple.classList.remove("animation");
+            }, 500)
+
+        }
+    }
+
+document.oncontextmenu=new Function("event.returnValue=false");
+document.onselectstart=new Function("event.returnValue=false");
